bba9f48e27f4655cd117257f1f5526e347c7f03f
Aiden Taylor <aiden.taylor@protonmail.com>
2023-04-18 10:21:05 -0600

    Initial commit.

---
+++ src/tools/binarypartitioner.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+
+// This function takes binarypath and returns the actual name of
+// the binary.
+void binarypartitioner(char* binarypath, char* binaryname) {
+
+  // We find the length of the binary's name with the last usage of '/'.
+  int binarynamelength = 0;
+  for(int i = strlen(binarypath); binarypath[i] != '/'; i--)
+    binarynamelength++;
+
+  // Then we write to the binaryname string with the proper info.
+  binaryname[0] = '.';
+  for(int i = 0; i < binarynamelength + 2; i++)
+    binaryname[i + 1] = binarypath[strlen(binarypath) - binarynamelength + i];
+
+  return;
+}
