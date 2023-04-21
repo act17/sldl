c8ce8585261318d98b43c4d7a1ba268afa36f3be
Aiden Taylor <aiden.taylor@protonmail.com>
2023-04-20 22:46:28 -0600

    Updated to now work with ``argumentparser()``.

--- src/main.c
+++ src/main.c
@@ -5,38 +5,50 @@
 
 int main() {
 
-  // This is the storage for the path to the binary.
-  char binarypath[256];
-  // This is the storage for the IWAD used.
-  char iwadpath[256];
-  // This is the storage for the binary.
-  char binaryname[256];
+  // First thing we need to do is open the files relating to configuration.
+  FILE* binsfilepath = fopen("bins.txt", "r");
+  FILE* iwadfilepath = fopen("iwad.txt", "r");
 
-  // Now we handle I/O for the rudamentary launcher:
-  printf("\nPlease enter the full path to your source port/binary of choice:\n");
-  scanf("%s",binarypath);
+  // Do some rudementary error handling - brush up on this in the future.
+  if(binsfilepath == NULL) {
+    printf("\nError! bins.txt does not exist.");
+    return 1;
+  }
+  if(iwadfilepath == NULL) {
+    printf("\nError! iwad.txt does not exist.");
+    return 1;
+  }
 
-  printf("\nPlease enter the full path to your IWAD of choice:\n");
-  scanf("%s",iwadpath);
+  // And then we call the variables that make up our binary and our IWAD.
+  char binary[64];
+  char iwad[64];
+  char argone[16];
 
-  // Then we define what the binary's name is.
-  binarypartitioner(binarypath, binaryname);
-
-  printf("\n");
-  printf("Binary path:\n %s \n",binarypath);
-  printf("Binary name:\n %s \n",binaryname);
-  printf("IWAD path:\n %s \n",iwadpath);
-  printf("\n");
-
-  // Then we set up the args used to launch the actual binary:
+  // We first define our Binary.
+  printf("\nPlease select your binary:");
+  argumentparser(binsfilepath, binary);
+  // Then we define our IWAD.
+  printf("\nPlease select your IWAD:");
+  argumentparser(iwadfilepath, iwad);
+  // We run the Binary's path through our parser:
+  binarypartitioner(binary, argone);
+  // And we set up our args:
   char* args[4];
-  args[0] = binaryname;
+  args[0] = argone;
   args[1] = "-iwad";
-  args[2] = iwadpath;
+  args[2] = iwad;
   args[3] = NULL;
+  // Close our files down:
+  fclose(binsfilepath);
+  fclose(iwadfilepath);
 
-  // And finally... we launch.
-  execvp(binarypath, args);
+  // Print out the nessicary information for diagnosis of what we're launching:
+  printf("\n\nBinary:	%s",binary);
+  printf("\nIwad:	%s",iwad);
+  for(int i = 0; i < 4; i++)
+    printf("\nargv[%d]:		%s",i,args[i]);
 
+  // And launch:
+  execvp(binary, args);
   return 0;
 }
