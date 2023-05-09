5972b77dd671c3a7e2c66a9d67a8a05ef503dc30
Aiden Taylor <aiden.taylor@protonmail.com>
2023-05-09 12:44:09 -0600

    Added headers for ``argselect()``.

--- src/sldl.h
+++ src/sldl.h
@@ -2,7 +2,7 @@
 #define __SLDL_H
 
 void binarypartitioner(char* binarypath, char* binaryname);
-
 void mainmenu(int Y, int X, char** args);
+void argselect(int Y, int X, char* filename, char* arg);
 
 #endif
