3d8b9cc885040de6bf18e0ad0f2138d8327de435
Aiden Taylor <aiden.taylor@protonmail.com>
2023-05-13 15:44:30 -0600

    Added ``int* quitcheck`` for ``mainmenu()``.

--- src/sldl.h
+++ src/sldl.h
@@ -2,7 +2,7 @@
 #define __SLDL_H
 
 void binarypartitioner(char* binarypath, char* binaryname);
-void mainmenu(int Y, int X, char** args);
+void mainmenu(int Y, int X, char** args, int* quitcheck);
 void argselect(int Y, int X, char* filename, char* arg);
 void infoscreen(int Y, int X);
 
