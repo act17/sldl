208c8fe7c89c3a90254c6e2f3bc16a7c5712f23b
Aiden Taylor <aiden.taylor@protonmail.com>
2023-05-15 15:45:44 -0600

    Added headers for ``fileinit.c``.

--- src/sldl.h
+++ src/sldl.h
@@ -1,7 +1,9 @@
 #ifndef __SLDL_H
 #define __SLDL_H
 
+int fileinit();
 void binarypartitioner(char* binarypath, char* binaryname);
+
 void mainmenu(int Y, int X, char** args, int* quitcheck);
 void argselect(int Y, int X, char* filename, char* arg);
 void infoscreen(int Y, int X);
