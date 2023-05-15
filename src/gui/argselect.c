827f8cb6a9acfe7e6259159ab876893ddb343d09
Aiden Taylor <aiden.taylor@protonmail.com>
2023-05-15 15:45:56 -0600

    Formatted.

--- src/gui/argselect.c
+++ src/gui/argselect.c
@@ -4,7 +4,8 @@
 #include "../sldl.h"
 
 // This function allows the user to select an argument.
-void argselect(int Y, int X, char* filename, char* arg) {
+void argselect(int Y, int X, char* filename, char* arg)
+{
 
   // Window init:
   init_pair(3,COLOR_WHITE,COLOR_BLACK);
@@ -28,11 +29,12 @@ void argselect(int Y, int X, char* filename, char* arg) {
   FILE* file = fopen(filename,"r");
   int linecount = 0;
 
-  while(feof(file) != true) {
-    fgets(buffer,64,file);
-    strcpy(arguments[linecount], buffer);
-    linecount++;
-  }
+  while(feof(file) != true)
+    {
+      fgets(buffer,64,file);
+      strcpy(arguments[linecount], buffer);
+      linecount++;
+    }
 
   fclose(file);
 
@@ -52,40 +54,44 @@ void argselect(int Y, int X, char* filename, char* arg) {
   int choice = 0;
   int highlight = 0;
   wattron(listwin,A_BOLD);
-  while(choice != 10) {
-    for(int i = 0; i < linecount - 1; i = i + 2) {
-      if(i == highlight)
-        wattron(listwin,A_REVERSE);
-      mvwprintw(listwin,1 + (i / 2), 2, "%s",arguments[i]);
-      wattroff(listwin,A_REVERSE);
-    }
+  while(choice != 10)
+    {
+      for(int i = 0; i < linecount - 1; i = i + 2)
+        {
+          if(i == highlight)
+            wattron(listwin,A_REVERSE);
+          mvwprintw(listwin,1 + (i / 2), 2, "%s",arguments[i]);
+          wattroff(listwin,A_REVERSE);
+        }
 
-    wrefresh(listwin);
-    wrefresh(stdscr);
+      wrefresh(listwin);
+      wrefresh(stdscr);
 
-    choice = wgetch(argwin);
-    switch (choice) {
-    case KEY_UP:
-      if(highlight == 0)
-        break;
-      highlight = highlight - 2;
-      break;
-    case KEY_DOWN:
-      if(highlight == linecount - 1)
-        break;
-      highlight = highlight + 2;
-      break;
-    default:
-      break;
+      choice = wgetch(argwin);
+      switch (choice)
+        {
+        case KEY_UP:
+          if(highlight == 0)
+            break;
+          highlight = highlight - 2;
+          break;
+        case KEY_DOWN:
+          if(highlight == linecount - 1)
+            break;
+          highlight = highlight + 2;
+          break;
+        default:
+          break;
+        }
     }
-  }
 
   // Then we need to run through the argument and ensure it's fixed:
   strcpy(arg,arguments[highlight + 1]);
-  for(int i = 0; arg[i] != '\0'; i++) {
-    if(arg[i] == '\n')
-      arg[i] = '\0';
-  }
+  for(int i = 0; arg[i] != '\0'; i++)
+    {
+      if(arg[i] == '\n')
+        arg[i] = '\0';
+    }
 
   // Then we delete the windows and return:
   wclear(argwin);
