b80ba08385827ee87d249244b2e3af1851156f43
Aiden Taylor <aiden.taylor@protonmail.com>
2023-05-16 13:07:58 -0600

    Added new features, including quitting pre-entry, and prevention of adding invalid entries.

--- src/gui/argselect.c
+++ src/gui/argselect.c
@@ -11,7 +11,7 @@ void argselect(int Y, int X, char* filename, char* arg)
   init_pair(3,COLOR_WHITE,COLOR_BLACK);
   WINDOW * argwin = newwin(36, 92, Y, X);
   WINDOW * listwin = newwin(18, 90, Y + 1, X + 1);
-  WINDOW * controlwin = newwin(4, 90, Y + 31, X + 1);
+  WINDOW * controlwin = newwin(5, 90, Y + 31, X + 1);
   wbkgd(argwin,COLOR_PAIR(1));
   wattron(argwin,COLOR_PAIR(1));
   wbkgd(listwin,COLOR_PAIR(3));
@@ -25,23 +25,23 @@ void argselect(int Y, int X, char* filename, char* arg)
 
   // File-related information gathering:
   char arguments[16][64];
-  char* buffer = malloc(64);
+  char* buffer = malloc(sizeof(char) * 64);
   FILE* file = fopen(filename,"r");
   int linecount = 0;
 
-  while(feof(file) != true)
-    {
-      fgets(buffer,64,file);
-      strcpy(arguments[linecount], buffer);
-      linecount++;
-    }
-
+  // We gather all entries from file, and then correct them.
+  while(feof(file) != true) {
+    fgets(buffer,64,file);
+    strcpy(arguments[linecount], buffer);
+    linecount++;
+  }
   fclose(file);
 
   // Printing information at bottom:
   wattron(controlwin,A_BOLD);
   mvwprintw(controlwin, 1, 1, "CONTROLS:");
-  mvwprintw(controlwin, 1, 1, "UP/DOWN/RETURN - SELECT ENTRY");
+  mvwprintw(controlwin, 2, 1, "UP/DOWN/RETURN - SELECT ENTRY");
+  mvwprintw(controlwin, 3, 1, "Q - RETURN TO PREVIOUS SCREEN");
 
   // Refreshing:
   refresh();
@@ -50,50 +50,72 @@ void argselect(int Y, int X, char* filename, char* arg)
   wrefresh(controlwin);
   wrefresh(stdscr);
 
-  // Input-output:
+  // Input-output operators:
   int choice = 0;
   int highlight = 0;
+  int quitcheck = 0;
   wattron(listwin,A_BOLD);
-  while(choice != 10)
-    {
-      for(int i = 0; i < linecount - 1; i = i + 2)
-        {
-          if(i == highlight)
-            wattron(listwin,A_REVERSE);
-          mvwprintw(listwin,1 + (i / 2), 2, "%s",arguments[i]);
-          wattroff(listwin,A_REVERSE);
-        }
+
+  // This upper-loop will only be broken when the selected argument is valid.
+  while(1) {
+    // This loop controls input-output related printing.
+    while(quitcheck != 1) {
+      for(int i = 0; i < linecount - 1; i = i + 2) {
+        if(i == highlight)
+          wattron(listwin,A_REVERSE);
+        mvwprintw(listwin,1 + (i / 2), 2, "%s",arguments[i]);
+        wattroff(listwin,A_REVERSE);
+      }
 
       wrefresh(listwin);
       wrefresh(stdscr);
 
       choice = wgetch(argwin);
-      switch (choice)
-        {
-        case KEY_UP:
-          if(highlight == 0)
-            break;
-          highlight = highlight - 2;
+      switch (choice) {
+      case KEY_UP:
+        if(highlight == 0)
           break;
-        case KEY_DOWN:
-          if(highlight == linecount - 1)
-            break;
-          highlight = highlight + 2;
+        highlight = highlight - 2;
+        break;
+      case KEY_DOWN:
+        if(highlight == linecount - 1)
           break;
-        default:
-          break;
-        }
+        highlight = highlight + 2;
+        break;
+      case 'q':
+        quitcheck = 1;
+        break;
+      case 10:
+        quitcheck = 1;
+      default:
+        break;
+      }
     }
 
-  // Then we need to run through the argument and ensure it's fixed:
-  strcpy(arg,arguments[highlight + 1]);
-  for(int i = 0; arg[i] != '\0'; i++)
-    {
+    // We first check to see if 'q' was given.
+    if(choice == 'q')
+      break;
+
+    // Otherwise, we see if the argument is valid after correcting it:
+    strcpy(arg,arguments[highlight + 1]);
+    for(int i = 0; arg[i] != '\0'; i++) {
       if(arg[i] == '\n')
         arg[i] = '\0';
     }
+    if(filevalidcheck(arg) == 0)
+      break;
 
-  // Then we delete the windows and return:
+    // In the case of invalidity, we set input variables to zero and print a warning message.
+    quitcheck = 0;
+    choice = 0;
+    wattron(argwin,COLOR_PAIR(4));
+    wattron(argwin,A_BOLD);
+    mvwprintw(argwin,29,1,"ERROR! Selected argument is not valid!");
+    wrefresh(argwin);
+    arg[0] = '\0';
+  }
+
+  // Then we delete the windows, free memory, and return:
   wclear(argwin);
   wclear(listwin);
   wclear(controlwin);
@@ -103,6 +125,7 @@ void argselect(int Y, int X, char* filename, char* arg)
   delwin(argwin);
   delwin(listwin);
   delwin(controlwin);
+  free(buffer);
 
   return;
 }
