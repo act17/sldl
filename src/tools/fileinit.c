e3d5a61403249cbf1ca89134941aa3b92b4d525c
Aiden Taylor <aiden.taylor@protonmail.com>
2023-05-16 13:07:12 -0600

    Formatted.

--- src/tools/fileinit.c
+++ src/tools/fileinit.c
@@ -9,28 +9,26 @@ int fileinit()
   char defaultpath[13] = "/path/to/file";
 
   FILE* bins = fopen("bins.txt","r");
-  if(!bins)
-    {
-      FILE* bins = fopen("bins.txt","w+");
-      printf("\nError!\n");
-      printf("bins.txt does not exist, and has been created in your directory with an example provided.\n");
-      fwrite(defaultname,1,13,bins);
-      fwrite(defaultpath,1,13,bins);
-      fclose(bins);
-      return 1;
-    }
+  if(!bins) {
+    FILE* bins = fopen("bins.txt","w+");
+    printf("\nError!\n");
+    printf("bins.txt does not exist, and has been created in your directory with an example provided.\n");
+    fwrite(defaultname,1,13,bins);
+    fwrite(defaultpath,1,13,bins);
+    fclose(bins);
+    return 1;
+  }
 
   FILE* iwad = fopen("iwad.txt","r");
-  if(!iwad)
-    {
-      FILE* iwad = fopen("iwad.txt","w+");
-      printf("\nError!\n");
-      printf("iwad.txt does not exist, and has been created in your directory with an example provided.\n");
-      fwrite(defaultname,1,13,iwad);
-      fwrite(defaultpath,1,13,iwad);
-      fclose(iwad);
-      return 1;
-    }
+  if(!iwad) {
+    FILE* iwad = fopen("iwad.txt","w+");
+    printf("\nError!\n");
+    printf("iwad.txt does not exist, and has been created in your directory with an example provided.\n");
+    fwrite(defaultname,1,13,iwad);
+    fwrite(defaultpath,1,13,iwad);
+    fclose(iwad);
+    return 1;
+  }
 
   fclose(iwad);
   fclose(bins);
