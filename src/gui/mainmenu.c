3a666be71eedd734c49cfebb582844af14500bab
Aiden Taylor <aiden.taylor@protonmail.com>
2023-05-22 18:41:45 -0600

    Updated to handle the addition of extra parameters.

--- src/gui/mainmenu.c
+++ src/gui/mainmenu.c
@@ -9,6 +9,8 @@ void mainmenu(int Y, int X, char** args, int* quitcheck)
   // Initializing variables:
   char* binarypath = malloc(sizeof(char) * 64);
   char* iwadpath = malloc(sizeof(char) * 64);
+  char* parameter = malloc(sizeof(char) * 256);
+  int extraargcount = 3;
 
   // Loop that controls when mainmenu() is to stop,
   // allows for recreation of appearance.
@@ -21,34 +23,45 @@ void mainmenu(int Y, int X, char** args, int* quitcheck)
     WINDOW * mainmenuwin = newwin(36,92,Y,X);
     WINDOW * binswin = newwin(3,90,Y+1,X+1);
     WINDOW * iwadwin = newwin(3,90,Y+4,X+1);
+    WINDOW * parawin = newwin(13,90,Y+7,X+1);
     WINDOW * controlwin = newwin(4,90,Y + 31,X+1);
     wbkgd(stdscr,COLOR_PAIR(2));
     wbkgd(mainmenuwin,COLOR_PAIR(1));
     wbkgd(binswin,COLOR_PAIR(1));
     wbkgd(iwadwin,COLOR_PAIR(1));
+    wbkgd(parawin,COLOR_PAIR(1));
     wbkgd(controlwin,COLOR_PAIR(1));
+
     wattron(mainmenuwin,COLOR_PAIR(1));
     wattron(binswin,COLOR_PAIR(1));
     wattron(iwadwin,COLOR_PAIR(1));
+    wattron(parawin,COLOR_PAIR(1));
     wattron(controlwin,COLOR_PAIR(1));
+
     box(mainmenuwin,0,0);
     box(binswin,0,0);
     box(iwadwin,0,0);
+    box(parawin,0,0);
     box(controlwin,0,0);
     keypad(mainmenuwin,true);
 
     // Printing info:
     wattron(binswin,A_BOLD);
     wattron(iwadwin,A_BOLD);
+    wattron(parawin,A_BOLD);
+    wattron(controlwin,A_BOLD);
     mvwprintw(binswin,1,1,"Binary path:");
     mvwprintw(iwadwin,1,1,"IWAD path:");
+    mvwprintw(parawin,1,1,"Extra Parameters:");
     wattroff(binswin,A_BOLD);
     wattroff(iwadwin,A_BOLD);
+    wattroff(parawin,A_BOLD);
     mvwprintw(binswin,1,14,"%s",binarypath);
     mvwprintw(iwadwin,1,14,"%s",iwadpath);
-    wattron(controlwin,A_BOLD);
-    mvwprintw(controlwin,1,1,"B - Select Binary | G - Select IWAD | RETURN - Play Doom!");
-    mvwprintw(controlwin,2,1,"I - Info Screen | Q - Quit SLDL");
+    for(int i = 0; i < 10; i++)
+      mvwprintw(parawin,i + 2,1,"%d: %s",i,args[i + 3]);
+    mvwprintw(controlwin,1,1,"B - Select Binary | G - Select IWAD | P - Enter Parameters");
+    mvwprintw(controlwin,2,1,"RETURN - Play Doom! | I - Info Screen | Q - Quit SLDL");
 
     // Printing error info in case of unfilled arguments.
     if(goodcheck == 0) {
@@ -64,6 +77,7 @@ void mainmenu(int Y, int X, char** args, int* quitcheck)
     wrefresh(mainmenuwin);
     wrefresh(binswin);
     wrefresh(iwadwin);
+    wrefresh(parawin);
     wrefresh(controlwin);
     wrefresh(stdscr);
 
@@ -75,6 +89,13 @@ void mainmenu(int Y, int X, char** args, int* quitcheck)
       break;
     case 'g':
       argselect(Y,X,"iwad.txt",iwadpath);
+      break;
+    case 'p':
+      echo();
+      mvwscanw(parawin,extraargcount - 1,4,"%s",parameter);
+      strcpy(args[extraargcount],parameter);
+      extraargcount++;
+      noecho();
       break;
     case 'i':
       infoscreen(Y, X);
@@ -105,7 +126,7 @@ void mainmenu(int Y, int X, char** args, int* quitcheck)
       break;
   }
 
-  // Exporting (Only occurs when 'q' is not specified):
+  // Exporting main arguments:
   if(userchoice != 'q') {
     strcpy(args[0],binarypath);
     strcpy(args[2],iwadpath);
@@ -114,6 +135,7 @@ void mainmenu(int Y, int X, char** args, int* quitcheck)
   // Freeing memory:
   free(binarypath);
   free(iwadpath);
+  free(parameter);
 
   return;
 }
