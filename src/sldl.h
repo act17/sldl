efb481ad7df37de198b3792f0cbd73c0db8eaa24
Aiden Taylor <aiden.taylor@protonmail.com>
2023-05-25 00:14:25 -0600

    Added headers for functions ``pwadselect()`` and ``paraselect()``.

--- src/sldl.h
+++ src/sldl.h
@@ -7,6 +7,8 @@ void binarypartitioner(char* binarypath, char* binaryname);
 
 void mainmenu(int Y, int X, char** args, int* quitcheck);
 void argselect(int Y, int X, char* filename, char* arg);
+void pwadselect(int Y, int X, char** pwads);
+void paraselect(int Y, int X, char** parameters);
 void infoscreen(int Y, int X);
 
 #endif
