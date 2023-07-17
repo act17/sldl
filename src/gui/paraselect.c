c47efb67207812dd713961fe35b4cdc71b266fe3
Aiden Taylor <aiden.taylor@protonmail.com>
2023-07-16 19:37:52 -0600

    Fixed a bug and added a new feature.

--- src/gui/paraselect.c
+++ src/gui/paraselect.c
@@ -11,7 +11,7 @@ void paraselect(int Y, int X, char** parameters)
 
   WINDOW * mainwin = newwin(36,92,Y,X);
   WINDOW * listwin = newwin(14,90,Y+1,X+1);
-  WINDOW * controlwin = newwin(4,90,Y+31,X+1);
+  WINDOW * controlwin = newwin(5,90,Y+30,X+1);
   wbkgd(mainwin,COLOR_PAIR(1));
   wbkgd(listwin,COLOR_PAIR(3));
   wbkgd(controlwin,COLOR_PAIR(1));
@@ -24,7 +24,8 @@ void paraselect(int Y, int X, char** parameters)
   keypad(mainwin,true);
   wattron(controlwin,A_BOLD);
   mvwprintw(controlwin,1,1,"Up/Down - Select Parameter | Return - Enter Parameter");
-  mvwprintw(controlwin,2,1,"D - Delete Parameter | C - Clear Parameters | Q - Exit");
+  mvwprintw(controlwin,2,1,"W - Shift Parameter Up | S - Shift Parameter Down");
+  mvwprintw(controlwin,3,1,"D - Delete Parameter | C - Clear Parameters | Q - Exit");
   wrefresh(mainwin);
   wrefresh(controlwin);
 
@@ -32,7 +33,8 @@ void paraselect(int Y, int X, char** parameters)
     for(int i = 0; i < 12; i++) {
       if(i == highlight)
         wattron(listwin,A_REVERSE);
-      mvwprintw(listwin,i+1,1,"%d: %s                                              ",i,parameters[i]);
+      mvwprintw(listwin,i+1,3,"                                                        ");
+      mvwprintw(listwin,i+1,1,"%d: %s",i,parameters[i]);
       wattroff(listwin,A_REVERSE);
     }
     wrefresh(listwin);
@@ -46,9 +48,23 @@ void paraselect(int Y, int X, char** parameters)
       highlight--;
       break;
     case KEY_DOWN:
-      if(highlight == 12)
+      if(highlight == 11)
         break;
       highlight++;
+      break;
+    case 'w':
+      if(highlight == 0)
+	break;
+      strcpy(buffer,parameters[highlight - 1]);
+      strcpy(parameters[highlight - 1],parameters[highlight]);
+      strcpy(parameters[highlight],buffer);
+      break;
+    case 's':
+      if(highlight == 11)
+	break;
+      strcpy(buffer,parameters[highlight + 1]);
+      strcpy(parameters[highlight + 1],parameters[highlight]);
+      strcpy(parameters[highlight],buffer);
       break;
     case 'd':
       strcpy(parameters[highlight],"\0");
@@ -60,7 +76,10 @@ void paraselect(int Y, int X, char** parameters)
     case 10:
       echo();
       mvwscanw(listwin,highlight+1,4,"%s",buffer);
-      strcpy(parameters[highlight],buffer);
+      if(buffer[0] != ' ')
+        strcpy(parameters[highlight],buffer);
+      else
+	strcpy(parameters[highlight],"\0");
       noecho();
       break;
     case 'q':
