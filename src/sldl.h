ad93eb641143f13f7ffe7c654ed84c8f48ac4fe3
Aiden Taylor <aiden.taylor@protonmail.com>
2023-09-09 18:44:07 -0600

    Updated headers.

--- src/sldl.h
+++ src/sldl.h
@@ -4,17 +4,18 @@
 int fileinit();
 int filevalidcheck(char* path);
 void binarypartitioner(char* binarypath, char* binaryname);
-void argumentsaver(char* binarypath, char* iwadpath, char** pwads, char** parameters);
-void argumentreader(char* binarypath, char* iwadpath, char** pwads, char** parameters);
+void argumentsaver(char* binarypath, char* iwadpath, char** pwads, char** parameters, char* basedir);
+void argumentreader(char* binarypath, char* iwadpath, char** pwads, char** parameters, char* basedir);
 void argumentcollapser(char** args, int size);
 
-void mainmenu(int Y, int X, char** args, int* quitcheck);
+void mainmenu(int Y, int X, char** args, int* quitcheck, char* basedir);
 void argselect(int Y, int X, char* filename, char* arg);
-void pwadselect(int Y, int X, char** pwads);
+void pwadselect(int Y, int X, char** pwads,char* pwadname);
 void paraselect(int Y, int X, char** parameters);
 void infoscreen(int Y, int X);
+void homedirsetup(char* dir);
 
-#define VERSION_ID "Release 1.2.0"
-#define VERSION_DATE "2023/07/16"
+#define VERSION_ID "Release 1.3.0"
+#define VERSION_DATE "2023/09/09"
 
 #endif
